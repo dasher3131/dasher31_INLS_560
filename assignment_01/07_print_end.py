bd8f9f78b76cdd83c36ce6b61993eba887305719
dasher3131 <143106205+dasher3131@users.noreply.github.com>
2025-09-08 19:30:08 -0400

    Add files via upload

---
+++ assignment_01/07_print_end.py
@@ -0,0 +1,1 @@
+# see 06_print_sep
---
+++ assignment_01/_03_handout.py
@@ -0,0 +1,20 @@
+
+# len
+
+# type
+
+# string
+
+# variable
+
+# CONSTANT
+
+# identifier
+
+# list
+
+# tuple
+
+# dict
+
+# set # this was not on the handout, but I added it in the video demo.
